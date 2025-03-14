8de57cae2f42195b53252ac2d55be49e9b1c835a
Szente Bence (Szf_N_2024_2026) <szente.bence@diak.szamalk-szalezi.hu>
2025-03-14 10:22:29 +0100

    kajdzsu

--- AB.php
+++ AB.php
@@ -1,6 +1,6 @@
 <?php
-class AB {
-    //adattagok
+class AB{
+    // adattagok
     private $host = "localhost";
     private $felhasznalo = "root";
     private $jelszo = "";
@@ -8,57 +8,77 @@ class AB {
     private $kapcsolat;
 
 
-    //konstruktor
-
+    // kontruktor
     public function __construct(){
+        // létrehozzuk a kapcsolatot
         $this->kapcsolat = new mysqli($this->host,
-        $this->felhasznalo, $this->jelszo, 
+        $this->felhasznalo,
+        $this->jelszo,
         $this->abNev);
 
         if($this->kapcsolat->connect_error){
             echo "<p>Sikertelen kapcsolódás.</p>";
         }else{
             echo "<p>Sikeres kapcsolódás.</p>";
-        }
-    } 
-    
+        }   
 
-    //tagfüggvények
+        $this->kapcsolat->query("SET NAMES UTF8");
+    }
+   
+    // tagfüggvények
     public function meret($tabla){
         $sql = "SELECT * FROM $tabla";
         return $this->kapcsolat->query($sql)->num_rows;
     }
-    
-    public function feltoltes($tabla,$mezo1,$mezo2){
-            $szinekSzama = $this->meret("szin");
-            $formaSzama = $this->meret("forma");
-            for ($i=1; $i <= $szinekSzama; $i++) { 
-                for ($j=1; $j <= $formaSzama; $i++) { 
-                   $sql = "INSERT INTO $tabla($mezo1, $mezo2) VALUES ('$i','$j')";
-                   $siker = $this->kapcsolat->query($sql);
-                   echo "$siker?\"siker\":\"nem siker\"";
-                }
-            }
-    }
-    
-    public function oszlopleker($oszlop,$table){
-        $sql="SELECT $oszlop FROM $table";
-        return $this->kapcsolat->query($sql);
+   
+    public function feltoltes($tabla, $mezo1, $mezo2){
+        $szinekSzama = $this->meret("szin");
+        $formakSzama = $this->meret("forma");
 
+        for ($i=1; $i <= $szinekSzama; $i++) {
+         
+            for ($j=1; $j <= $formakSzama ; $j++) {
+            $sql = "INSERT INTO $tabla($mezo1, $mezo2)
+            VALUES ('$i','$j')";
+            $siker = $this->kapcsolat->query($sql);
+            echo $siker?"siker":"nem siker";
+          }
+        }
     }
-    
+
+
+    public function oszlopLeker($oszlop,$tabla) {
+        $sql = "SELECT $oszlop FROM $tabla";
+        //Mátrix eredmény
+        return $this->kapcsolat->query($sql);
+    }
+   
+   
+    public function oszlopleker2($oszlop,$oszlop2,$tabla){
+        $sql = "SELECT $oszlop, $oszlop2 FROM $tabla";
+        return $this->kapcsolat->query($sql);
+    }
+
+
+
     public function megjelenit($matrix){
+        while ($row = $matrix -> fetch_row() ){
+            echo "<img src='forras/$row[0]'alt ='kártya képei'>";
+        }
+   
     }
+   
+    public function tableone($matrix){
+        while($row = $matrix -> fetch_row()){
+            echo "<tr><td><p>$row[0]</p></td><td><img src='forras/$row[1]' alt='kártya képei'></td></tr>";
+        }
+        echo "</table>";    
+    }
+
+
     public function bezar(){
         $this->kapcsolat->close();
     }
-
-
-
-
-
-
-
 
 }
 ?>
--- index.php
+++ index.php
@@ -1,27 +1,36 @@
 <?php
-    include_once "AB.php";
+    include_once "AB.php"
 ?>
-
-
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="hu">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stilus.css">
     <title>Kártya</title>
 </head>
 <body>
-    <?php    
-    $adatbazis= new AB();
+    <?php
+        $adatbazis = new AB();
+        if($adatbazis->meret("kartya") == 0){
+            $adatbazis->feltoltes("kartya", "szinAzon", "formaAzon");
+        }
+        $matrix = $adatbazis->oszlopLeker2("nev","kep", "szin");
+        echo "<table>";
+        echo "<tr>
+                <th>
+                    <p>Név</p>
+                </th>
+                <th>
+                    <p>Kép</p>
+                </th>
+              </tr>";
+        
+        $adatbazis->tableone($matrix);
+        $adatbazis->bezar();
+        
 
-    if($adatbazis->meret("kartya")==0){
-
-    $adatbazis->feltoltes("kartya","formAzon","szinAzon");
-    }
-    $matrix = $adatbazis->oszlopleker("kep","szin")
-    $adatbazis->megjelenit($matrix);
-    $adatbazis->bezar();
+        
 
     ?>
 </body>
